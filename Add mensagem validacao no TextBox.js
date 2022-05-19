0bc51f32a93a79df721ad313272348814e959c2b
Marcos Caetano <39209992+MCaetanoPJ@users.noreply.github.com>
2022-05-19 16:34:52 -0300

    Add método para remover as mensagens de validação

--- Add mensagem validacao no TextBox.js
+++ Add mensagem validacao no TextBox.js
@@ -4,15 +4,33 @@
 //id = id do elemento onde o texto será inserido
 //mensagem = texto que será inserido
 
-function addMensagemValidacao(id, mensagem){
-var campo = document.getElementById(id);
-var controls = campo.parentNode;
+function addMensagemValidacao(id, mensagem) {
+    removeMensagemValidacao(id);
 
-var div_error = document.createElement("div");
-div_error.className = "error";
-div_error.innerHTML = '<p class="error help-block"><span class="label label-important">' + mensagem + '</span></p>';
+    var campo = document.getElementById(id);
+    if (campo != null) {
+        var controls = campo.parentNode;
 
-controls.appendChild(div_error);
+        var div_error = document.createElement("div");
+        div_error.className = "error";
+        div_error.innerHTML = '<p class="error help-block"><span class="label label-important">' + mensagem + '</span></p>';
+
+        controls.appendChild(div_error);
+    }
+}
+
+function removeMensagemValidacao(id) {
+    var campo = document.getElementById(id);
+    if (campo != null) {
+        var controls = campo.parentNode;
+        var divErros = controls.getElementsByClassName('error');
+        var count = divErros.length;
+        for (var i = 0; i < count; i++) {
+            if (divErros[i] != null) {
+                divErros[i].remove();
+            }
+        }
+    }
 }
 
 //Exemplo de uso da function addMensagemValidacao: 
