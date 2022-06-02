90fdebe1b7310f3a8344d5d31d93dedaf0e54d2a
Marcos Caetano <39209992+MCaetanoPJ@users.noreply.github.com>
2022-06-02 13:47:28 -0300

    Update Controlar ToggleButton.js
    Adicionando function anônima para monitorar o ToggleButton

--- Controlar ToggleButton.js
+++ Controlar ToggleButton.js
@@ -1,3 +1,18 @@
+//Function JS anonima para monitorar o ToggleButton
+$(function () {
+    $('#IdToggleButton').change(function () {
+        var classe = "";
+        var txtPlaceholter = "";
+        if ($(this).attr("checked") === "checked") {
+            //ToggleButton marcado
+        }
+        else {
+            //ToggleButton desmarcado
+        }
+    });
+});
+
+//Function para alterar visualmente o ToggleButton, o value não será alterado
 function marcarToggleButtonByClass(nomeClass, ativar){
 	var toggle = document.getElementsByClassName(nomeClass);
 	if(toggle != null){
